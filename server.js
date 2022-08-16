ec8464fc7787d27aaf458cf853bcf663337b038b
Tom Wilson <tom@hyper.io>
2022-08-16 14:13:33 +0000

    added endpoint to setup warp-cache

--- server.js
+++ server.js
@@ -8,6 +8,22 @@ const app = express()
 
 app.use(cors({ credentials: true }))
 
+app.get('/:contract', async (req, res) => {
+  if (!req.params.contract) {
+    return res.status(400).send({ message: 'no contract specified' })
+  }
+  try {
+    const result = await warp.contract(req.params.contract)
+      .setEvaluationOptions({
+        allowUnsafeClient: true,
+        allowBigInt: true
+      }).readState()
+    res.send(result.cachedValue.state)
+  } catch (e) {
+    res.status(404).send({ message: 'not found!' })
+  }
+})
+
 app.get('/', async (req, res) => {
   const result = await warp.contract('9nDWI3eHrMQbrfs9j8_YPfLbYJmBodgn7cBCG8bii4o')
     .setEvaluationOptions({
