44b31c96c7dca4f7752929933d840830cfe86605
Tom Wilson <tom@hyper.io>
2023-07-18 23:01:35 +0000

    update sw server without fpjson

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const path = require('path')
 const express = require('express')
 const cors = require('cors')
-const fpjson = require('fpjson-lang')
 const { JSONPath } = require('jsonpath-plus')
 
 const { WarpFactory, LoggerFactory, defaultCacheOptions } = require('warp-contracts')
@@ -36,7 +35,7 @@ app.get('/contract', async (req, res) => {
       })
       return
     }
-    //console.log(result.cachedValue.errors)
+
     res.send({
       sortKey: result.sortKey,
       state: result.cachedValue.state,
@@ -44,7 +43,7 @@ app.get('/contract', async (req, res) => {
       errorMessages: result.cachedValue.errorMessages,
       status: "evaluated"
     })
-    //res.send(result.state)
+
   } catch (e) {
     console.log(e)
     res.status(404).send({ message: 'not found!' })
@@ -63,33 +62,22 @@ app.get('/:contract', async (req, res) => {
         internalWrites: true,
         useVM2: true
       }).readState()
+
+    if (req.query.query) {
+      const queryResult = JSONPath({ path: req.query.query, json: result.cachedValue.state });
+      res.send({
+        sortKey: result.sortKey,
+        result: queryResult,
+        validity: result.cachedValue.validity,
+        status: "evaluated"
+      })
+      return
+    }
     //console.log(result.cachedValue.errors)
-    res.send(result.cachedValue.state)
+    res.send(result.cachedValue.state.balances)
     //res.send(result.state)
   } catch (e) {
     console.log(e)
-    res.status(404).send({ message: 'not found!' })
-  }
-})
-
-app.post('/:contract', express.json(), async (req, res) => {
-  if (!req.params.contract) {
-    return res.status(400).send({ message: 'no contract specified' })
-  }
-  try {
-    const result = await warp.contract(req.params.contract)
-      .setEvaluationOptions({
-        unsafeClient: 'skip',
-        allowBigInt: true,
-        internalWrites: true,
-        useVM2: true
-      }).readState()
-    //console.log(result.cachedValue.errors)
-    const fpresult = fpjson.default([req.body, result.cachedValue.state])
-    res.send({ result: fpresult })
-    //res.send(result.state)
-  } catch (e) {
-    console.log(e.message)
     res.status(404).send({ message: 'not found!' })
   }
 })
