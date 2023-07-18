26ed018a236524c08043c850caef4758e57f490c
Tom Wilson <tom@hyper.io>
2023-07-18 23:07:09 +0000

    bootstrap server

--- readstate.js
+++ readstate.js
@@ -1,7 +1,7 @@
 const { WarpFactory, LoggerFactory, defaultCacheOptions } = require('warp-contracts')
 
 const STAMP = 'TlqASNDLA1Uh8yFiH-BzR_1FDag4s735F3PoUFEv2Mo'
-const BAR = 'KTzTXT_ANmF84fWEKHzWURD1LWd9QaFR9yfYUwH2Lxw'
+const U = 'KTzTXT_ANmF84fWEKHzWURD1LWd9QaFR9yfYUwH2Lxw'
 const UCM = 'pKAUV26rFgG13XwS7oZ1IQ8dDIRcdV9xnC8XEnZ7cfQ'
 
 const HEIGHT = 1200000
@@ -9,8 +9,9 @@ const warp = WarpFactory.forMainnet()
 async function main() {
 
   await readState(UCM)
+  await readState(U)
   //  await readState(STAMP)
-  //  await readState(BAR)
+
 }
 
 main()
